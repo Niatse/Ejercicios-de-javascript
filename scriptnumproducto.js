6d3e9ef41c1ad5d4b878e96818ceec3a744a9afe
Niatse <75042389+Niatse@users.noreply.github.com>
2021-02-05 03:01:08 -0300

    Add files via upload

---
+++ scriptnumproducto.js
@@ -0,0 +1,9 @@
+var codigo = Number(prompt("Digite el codigo del producto"));;
+
+if (codigo ==123) 
+	alert("El precio del samsung galaxy S20 es de 1000USD")
+  else if (codigo ==1234)
+     alert("El precio del Xiaomi MI 9 es de 400USD")
+  else
+  	alert("El producto no existe")
+
---
+++ scriptperimetro.js
@@ -0,0 +1,8 @@
+var lado1 = parseInt(prompt("Ingrese la primer medida"))
+var lado2 = parseInt(prompt("Ingrese la segunda medida"))
+var lado3 = parseInt(prompt("Ingrese la tercera medida"))
+var lado4 = parseInt(prompt("Ingrese la cuarta medida"))
+
+ var Perimetro=(lado1)+(lado2)+(lado3)+(lado4)
+
+alert("El perimetro de la figura es: "+Perimetro)
